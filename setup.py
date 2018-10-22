18c4f43ed10e9038a3b0353a7f664feec7f0dd8d
Tom Gross <itconsense@gmail.com>
2018-10-22 09:08:18 +0200

    Trove classifier

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],

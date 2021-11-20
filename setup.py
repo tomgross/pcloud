5974fa53d9b50f56ac4e16789607cff0d85025e2
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-20 13:33:14 +0530

    2 to 1 line

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the pcloud package."""
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 long_description = '\n\n'.join([

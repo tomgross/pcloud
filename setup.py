faeccac3b3bdb0ddccce9b0cadfa41dce3a7c910
Tom Gross <itconsense@gmail.com>
2023-12-22 13:22:45 +0100

    Fix Playwright locators in test

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         "requests",
         "requests-toolbelt",
-        "setuptools"
+        "setuptools>=68.0.0"
     ],
     extras_require={"pyfs": ["fs"]},
     entry_points={
